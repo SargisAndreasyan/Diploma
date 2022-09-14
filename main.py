7b6e4d9d44f33cf6e6cbee4b1e51bc154466b85f
Sargis <saqozzz1970@gmail.com>
2022-09-14 17:29:09 +0400

    added hand cords

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import cv2
 import mediapipe as mp
 
 
+def get_cords(image):
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for i, cords in enumerate(handLms.landmark):
+                if i == 20:
+                    print(i, cords.x)
+
+
 def handsFinder(image, draw=True):
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(imageRGB)
@@ -11,6 +21,7 @@ def handsFinder(image, draw=True):
 
             if draw:
                 mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+
     return image
 
 
@@ -23,7 +34,12 @@ while True:
     success, img = cap.read()
     imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hands.process(imageRGB)
-    cv2.imshow('img', handsFinder(imageRGB))
+    get_cords(img)
+    img = handsFinder(img)
+    cv2.imshow('img', img)
 
     if cv2.waitKey(33) == 27:
         break
+
+cap.release()
+cv2.destroyAllWindows()
