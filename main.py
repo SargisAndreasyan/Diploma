ab2e5085af55ec5981c102b2be661167dc941757
Sargis <saqozzz1970@gmail.com>
2022-09-18 16:21:12 +0400

    Changed to class based app Added mouse

--- main.py
+++ main.py
@@ -1,45 +1,79 @@
 import cv2
 import mediapipe as mp
+import pyautogui as pg
 
 
-def get_cords(image):
-    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(imageRGB)
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for i, cords in enumerate(handLms.landmark):
-                if i == 20:
-                    print(i, cords.x)
+class CompEye:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
+
+    @staticmethod
+    def convert_to_rgb(img):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    @staticmethod
+    def show_img(title, img):
+        cv2.imshow(title, img)
+
+    @staticmethod
+    def get_hands(results):
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                yield handLms
+
+    def read_cap(self):
+        "success_flag and frame"
+        return self.cap.read()
+
+    def hand_detection(self, image_rgb):
+        return self.hands.process(image_rgb)
+
+    def start_show(self):
+        while True:
+            success, img = self.read_cap()
+            image_rgb = self.convert_to_rgb(img)
+            result = self.hand_detection(image_rgb)
+            img = self.hands_drawing(img, result)
+            self.show_img('Frame', img)
+            if cv2.waitKey(33) == 27:
+                break
+
+    def get_cords(self, finger):
+        success, img = self.read_cap()
+        image_rgb = self.convert_to_rgb(img)
+        result = self.hand_detection(image_rgb)
+        if result.multi_hand_landmarks:
+            for hand in result.multi_hand_landmarks:
+                for i, cord in enumerate(hand.landmark):
+                    if finger == i:
+                        return cord.x, cord.y
+
+    def hands_drawing(self, image, results):
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
+        return image
 
 
-def handsFinder(image, draw=True):
-    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(imageRGB)
+class Mouse(CompEye):
+    def __init__(self):
+        CompEye.__init__(self)
+        pg.FAILSAFE = False
+        self.width, self.height = pg.size()
+        pg.dragTo(self.width // 2, self.height // 2)
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
+    def show(self):
+        while True:
+            cords = self.get_cords(8)
+            if cords is not None:
+                x, y = cords
+                x = 1 - x
+                pg.moveTo(x * self.width, y * self.height)
 
-            if draw:
-                mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
 
-    return image
-
-
-cap = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
-
-while True:
-    success, img = cap.read()
-    imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result = hands.process(imageRGB)
-    get_cords(img)
-    img = handsFinder(img)
-    cv2.imshow('img', img)
-
-    if cv2.waitKey(33) == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    mouse = Mouse()
+    mouse.show()
---
+++ test.py
@@ -0,0 +1,47 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+import pyautogui as pg
+import time
+pyautogui.FAILSAFE=False
+width,height =  pyautogui.Size()
+def get_cords(image):
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for i, cords in enumerate(handLms.landmark):
+                if i == 20:
+                    pg.moveRel(cords.x*width,cords.y*height)
+
+
+def handsFinder(image, draw=True):
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+
+            if draw:
+                mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+
+    return image
+
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imageRGB)
+    get_cords(img)
+    cv2.imshow('img', img)
+
+    if cv2.waitKey(33) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
