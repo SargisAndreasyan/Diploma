6370022c6d4a127d817f31585279700d14df9735
Sargis <andreeasyan.sargis@gmail.com>
2022-11-28 22:59:45 +0300

    Added pack classes with __init__.py witch contain CompEye and Mouse classes. Solved class divide problem.

---
+++ classes/mouse.py
@@ -0,0 +1,17 @@
+from comp_eye import CompEye
+import pyautogui as pg
+
+
+class Mouse(CompEye):
+    def __init__(self):
+        CompEye.__init__(self)
+        pg.FAILSAFE = False
+        self.width, self.height = pg.size()
+
+    def show(self):
+        while True:
+            cords = self.get_cords(8)
+            if cords is not None:
+                x, y = cords
+                x = 1 - x
+                pg.moveTo(x * self.width, y * self.height)
